db8fc4f432918012e09cf0758fa1eeb9df0d8d18
hissan1983 <hissan1983@gmail.com>
2014-08-21 17:09:47 +0900

    ルートパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'areas#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
